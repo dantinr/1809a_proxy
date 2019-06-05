852f78b4c9cd89ccd10eba4a799f98a9d56d848b
dante <dantinr@gmail.com>
2019-06-05 01:09:04 +0000

    debug

--- user.php
+++ user.php
@@ -5,7 +5,7 @@
 
     echo '<pre>';print_r($_SESSION);echo '</pre>';echo '<hr>';
 
-    if(isset($_SESSION['user_name']){
+    if(isset($_SESSION['user_name'])){
         echo '已登录';
     }else{
         echo '请登录';
